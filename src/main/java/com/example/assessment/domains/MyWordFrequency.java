481816f65c9fd8e4286e5912e39e3ec1777472ef
elvissomers <elvissomers98@gmail.com>
2023-04-04 11:10:53 +0200

    Create MyWordFrequency.java

---
+++ src/main/java/com/example/assessment/domains/MyWordFrequency.java
@@ -0,0 +1,17 @@
+package main.java.com.example.assessment.domains;
+
+public class MyWordFrequency implements WordFrequency{
+
+    private String word;
+
+    private int frequency;
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+}
